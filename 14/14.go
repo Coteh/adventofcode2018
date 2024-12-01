ae3e132841d2a35360b4c23b267887d8f70985fc
James Cote <3276350+Coteh@users.noreply.github.com>
2024-11-30 21:57:42 -0500

    Day 14 part 2

--- 14/14.go
+++ 14/14.go
@@ -12,7 +12,7 @@ import (
 
 var debugFlag *bool
 
-func parseInput() (numRecipes int) {
+func parseNumRecipesInput() (numRecipes int) {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for scanner.Scan() {
@@ -23,6 +23,22 @@ func parseInput() (numRecipes int) {
 		if err != nil {
 			log.Fatal(err)
 		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return
+}
+
+func parseSequenceInput() (sequence string) {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		sequence = line
 	}
 
 	if err := scanner.Err(); err != nil {
@@ -76,6 +92,68 @@ func iterateReceipesRight(recipeArr []int, startingPos []int, checkAfter int) (s
 	return string(scoreStr)
 }
 
+func iterateReceipesLeft(recipeArr []int, startingPos []int, sequence string) (numRecipes int) {
+	if len(recipeArr) == 1 || len(startingPos) == 1 {
+		fmt.Fprintf(os.Stderr, "Invalid params")
+		return
+	}
+
+	sequenceNums := make([]int, 0, len(sequence))
+	for _, val := range sequence {
+		sequenceNums = append(sequenceNums, int(val - '0'))
+	}
+	if *debugFlag {
+		fmt.Println(sequenceNums)
+	}
+
+	currPos := startingPos
+mainLoop:
+	for {
+		if *debugFlag {
+			printRecipeState(recipeArr, currPos)
+		}
+		sum := 0
+		for _, pos := range currPos {
+			sum += recipeArr[pos]
+		}
+		sumStr := strconv.FormatInt(int64(sum), 10)
+		arr := strings.Split(sumStr, "")
+		for _, val := range arr {
+			num, err := strconv.Atoi(val)
+			if err != nil {
+				panic(err)
+			}
+			recipeArr = append(recipeArr, num)
+		}
+		// Realized after reading this that the last digit of the input sequence can be missed if the last recipe generated was two digits.
+		// https://www.reddit.com/r/adventofcode/comments/a671s8/comment/ebskkoy/
+		// To fix this edge case, the starting point of the check needs to start not just from the last digit, but also from the digit before that if 2 digits were generated.
+		// This loop will perform a check from each valid starting point (it should only ever be from 1 or 2 digits from the end of scoreboard)
+		for k := 1; k <= len(arr); k++ {
+			start := len(recipeArr) - k
+			j := len(sequence) - 1
+			for i := start; i >= 0 && j >= 0; i-- {
+				if recipeArr[i] == sequenceNums[j] {
+					j--
+				} else {
+					break
+				}
+			}
+			// If sequence was detected, then count number of digits (recipes) before the sequence and return that
+			if (j < 0) {
+				numRecipes = len(recipeArr) - k + 1 - len(sequence)
+				break mainLoop
+			}
+		}
+		for i, pos := range currPos {
+			diff := 1 + recipeArr[pos]
+			currPos[i] = (currPos[i] + diff) % len(recipeArr)
+		}
+	}
+
+	return
+}
+
 func printRecipeState(recipeArr []int, currPos []int) {
 	for i, recipe := range recipeArr {
 		if currPos[0] == i {
@@ -91,13 +169,34 @@ func printRecipeState(recipeArr []int, currPos []int) {
 
 func main() {
 	debugFlag = flag.Bool("debug", false, "Turn on debug options")
-	flag.Parse()
-
-	numRecipes := parseInput()
-	if *debugFlag {
-		fmt.Println("numRecipes", numRecipes)
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "usage: %s [flags] <left | right>\n\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Flags:\n")
+		flag.PrintDefaults()
 	}
-
-	scoreStr := iterateReceipesRight([]int{3, 7}, []int{0, 1}, numRecipes)
-	fmt.Println(scoreStr)
+	flag.Parse()
+	
+	direction := flag.Arg(0)
+	
+	switch (direction) {
+		// Part 1
+		case "right":
+			numRecipes := parseNumRecipesInput()
+			if *debugFlag {
+				fmt.Println("numRecipes", numRecipes)
+			}
+			scoreStr := iterateReceipesRight([]int{3, 7}, []int{0, 1}, numRecipes)
+			fmt.Println(scoreStr)
+		// Part 2
+		case "left":
+			sequence := parseSequenceInput()
+			if *debugFlag {
+				fmt.Println("sequence", sequence)
+			}
+			numRecipes := iterateReceipesLeft([]int{3, 7}, []int{0, 1}, sequence)
+			fmt.Println(numRecipes)
+		default:
+			fmt.Fprintln(os.Stderr, "Please provide direction (\"left\" or \"right\")")
+			os.Exit(1)
+	}
 }
--- 14/14_test.go
+++ 14/14_test.go
@@ -19,11 +19,38 @@ func TestPart1(t *testing.T) {
 		scoreStr := iterateReceipesRight(recipeArr, []int{0, 1}, input)
 
 		if scoreStr != expected {
-			t.Fatalf("'%d' case does not match, expected: '%s', actual: '%s'", input, scoreStr, expected)
+			t.Fatalf("'%d' case does not match, expected: '%s', actual: '%s'", input, expected, scoreStr)
 		}
 	}
 }
 
 func TestPart2(t *testing.T) {
-	t.Skip("TODO: Finish Part 2")
+	recipeArr := []int{3, 7}
+
+	var testDebugFlag = false
+	debugFlag = &testDebugFlag
+
+	type TestCase struct {
+		name string
+		input string
+		expected int
+	}
+
+	testCases := []TestCase{
+		{"sample 1", "51589", 9},
+		{"sample 2", "01245", 5},
+		{"sample 3", "92510", 18},
+		{"sample 4", "59414", 2018},
+		{"https://www.reddit.com/r/adventofcode/comments/a671s8/2018_day_14_part_2_i_dont_know_why_my_answer_is/", "147061", 20283721},
+		{"input", "409551", 20219475},
+		{"last digit of input is the second digit of recipe sum", "5158916", 9},
+	}
+
+	for _, testCase := range testCases {
+		numRecipes := iterateReceipesLeft(recipeArr, []int{0, 1}, testCase.input)
+
+		if numRecipes != testCase.expected {
+			t.Fatalf("case \"%s\" ('%s') does not match, expected: %d, actual: %d", testCase.name, testCase.input, testCase.expected, numRecipes)
+		}
+	}
 }
